98029077386776f92352b31bd8de0204cbe51e4c
Inkyu-Lee <inkyulee9704@gmail.com>
2024-09-22 17:10:34 +0900

    First Commit

---
+++ typing-sprint-dev/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App:React.FC = () => {
+  return (
+    <>
+      <div className='text-3xl font-bold underline'>
+        <p className='text-3xl font-bold underline'>Hello World!</p>
+        <h1>헬로월드</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
